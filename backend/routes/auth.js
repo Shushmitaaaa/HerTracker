9a5847e6d873274b8c0ba3a0a1bceb0ac86cfa21
Shushmita <Shushmitat39@gmail.com>
2026-01-04 21:35:24 +0530

    added secure login using JWT

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -25,4 +25,25 @@ router.post('/register', async (req, res) => {
   }
 });
 
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    
+    let user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ msg: "Invalid Credentials" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid Credentials" });
+    
+    const payload = { user: { id: user.id } };
+    jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '24h' }, (err, token) => {
+      if (err) throw err;
+      res.json({ token });
+    });
+  } catch (err) {
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
