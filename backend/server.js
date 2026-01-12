bd4aceb2921b0d7fab83c1767991bd2035e55475
Shushmita <Shushmitat39@gmail.com>
2026-01-12 12:08:57 +0530

    updated CORS middleware

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,13 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    process.env.FRONTEND_URL
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 
@@ -20,5 +26,5 @@ mongoose.connect(process.env.MONGO_URI)
 const PORT = process.env.PORT || 5000;
 app.use('/api/auth', require('./routes/auth'));
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
